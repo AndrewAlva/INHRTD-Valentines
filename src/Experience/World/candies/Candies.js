ef0e1c4e271ae746dba6c18fcb9d31529f4cf0e5
Andrew Alvarado <andrew@activetheory.net>
2025-01-22 16:06:50 -0600

    Scaffold: Device real orientation to modify 3d scene testing

--- src/Experience/World/candies/Candies.js
+++ src/Experience/World/candies/Candies.js
@@ -4,10 +4,12 @@ import Candy1 from './Candy1.js'
 import Candy2 from './Candy2.js'
 import Candy3 from './Candy3.js'
 
+let _this;
 export default class Candies
 {
     constructor()
     {
+        _this = this;
         this.experience = new Experience()
         this.appState = this.experience.appState
         this.device = this.experience.device
@@ -54,7 +56,61 @@ export default class Candies
                 this.secondCandy.mesh.visible = false;
             }
         });
+
+        this.initDeviceOrientation();
     }
+
+    initDeviceOrientation() {
+        this.tester = document.createElement('div');
+        this.tester.style.width = '80%';
+        this.tester.style.display = 'block';
+        this.tester.style.position = 'fixed';
+        this.tester.style.top = '20px';
+        this.tester.style.left = '20px';
+        this.tester.style.fontSize = '14px';
+        this.tester.style.backgroundColor = 'rgba(0,0,0, 0.2)';
+        this.tester.style.zIndex = 999999999;
+        document.body.appendChild(this.tester);
+        this.tester.innerHTML = `${DeviceOrientationEvent.requestPermission}`;
+
+        if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+            DeviceOrientationEvent.requestPermission()
+                .then((permissionState) => {
+                    if (permissionState === 'granted') {
+                        console.log('permission granted');
+                        
+                        window.addEventListener('deviceorientation', (event) => {
+                            _this.handleOrientation(event);
+                        });
+                    } else {
+                        console.log('Permission denied for device orientation.');
+                        this.tester.innerHTML = 'Permission denied for device orientation.';
+                    }
+                })
+                .catch(console.error);
+        } else {
+            // Non-iOS devices
+            console.log('Non-iOS devices');
+            window.addEventListener('deviceorientation', (event) => {
+                _this.handleOrientation(event);
+            });
+        }
+
+        // window.addEventListener("deviceorientation", this.handleOrientation.bind(this), true);
+    }
+
+    handleOrientation(event) {
+        console.log(event);
+        console.log(`rotateDegrees = ${event.alpha};<br>leftToRight = ${event.gamma};<br>frontToBack = ${event.beta};`);
+        _this.tester.innerHTML = `Alpha: ${event.alpha}. Beta: ${event.beta}. Gamma: ${event.gamma}`;
+
+        _this.group.position.x = event.alpha * 0.1;
+        _this.group.position.y = event.beta * 0.1;
+        _this.group.position.z = event.gamma * 0.1;
+
+    }
+
+
 
     update()
     {
