717c554d563c5df61f47ac75cc23c7965fba036a
Andrew Alvarado <andrew@activetheory.net>
2025-01-25 20:03:25 -0600

    Dark scene: radial bg tweaks to match figma comp

--- src/Experience/shaders/bgDark/frag.glsl
+++ src/Experience/shaders/bgDark/frag.glsl
@@ -4,12 +4,24 @@ uniform float uTransition;
 
 varying vec2 vUv;
 
+vec2 rotateUV(vec2 uv, float r, vec2 origin) {
+    float c = cos(r);
+    float s = sin(r);
+    mat2 m = mat2(c, -s,
+                  s, c);
+    vec2 st = uv - origin;
+    st = m * st;
+    return st + origin;
+}
+
 void main()
 {
-    float start = 0.02;
-    float fade = 0.15;
+    vec2 bgUV = rotateUV(vUv, -0.5, vec2(0.5));
+    bgUV = bgUV * vec2(1., 1.3);
+    float start = 0.01;
+    float fade = 0.12;
 
-    float circle = length(vUv - vec2(0.5, 0.48));
+    float circle = length(bgUV - vec2(0.485, 0.725));
 
     float lerp = smoothstep(start, start + fade, circle);
     vec3 color = mix(uColorInside, uColorOutside, lerp);
