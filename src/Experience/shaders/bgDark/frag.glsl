b6de5d6c5752d171ed05b7ab5af2e0614fd233a1
Andrew Alvarado <andrew@activetheory.net>
2025-02-04 12:34:25 -0600

    clean up dark bg shader

--- src/Experience/shaders/bgDark/frag.glsl
+++ src/Experience/shaders/bgDark/frag.glsl
@@ -39,7 +39,7 @@ void main()
 {
     vec2 screenUV = gl_FragCoord.xy / uScreen;
 
-    // Radial gradient bg
+    // "Light" Radial gradient bg
     vec2 bgUV = rotateUV(vUv, -0.5, vec2(0.5));
     bgUV = bgUV * vec2(1., 1.3);
     float start = 0.01;
@@ -47,7 +47,6 @@ void main()
     float textFade = 0.22;
 
     float circle = length(bgUV - vec2(0.485, 0.725));
-
     float lerp = smoothstep(start, start + fade, circle);
     vec3 gradient = mix(uColorInside, uColorOutside, lerp);
 
@@ -59,60 +58,45 @@ void main()
 
 
     // Text UVs (including distortion)
-    vec2 tileUV = abs(vUv - vec2(1., 0.));
+    // vec2 tileUV = abs(vUv - vec2(1., 0.));
     vec2 textUV = screenUV;
 
 
     // Perlin Noise calc
-    vec4 displacement = texture2D(uDisplacementMap, screenUV);
+    float displacement = texture2D(uDisplacementMap, screenUV).r;
     displacement = 1. - displacement;
-    float strength = pow(displacement.r, 0.8) * 1.125;
-    // float strength = displacement.r + 0.75;
-    // float strength = displacement.r + 1.175;
-    // textUV.x += strength;
+    float strength = pow(displacement, 0.8) * 1.125;
 
 
     // Waves
-    // float waves = fract((screenUV.y * 10.) + cos(screenUV.x * 20.) * 0.3);
     float waves = screenUV.y - (cos(screenUV.x * PI_2 * 2.) * 0.175);
     waves *= strength * 0.5;
     textUV.y += waves;
-    // textUV.y *= 0.5;
 
 
     // Radial Circle
-    // float radialCircle = 1. - (pow((1. - length(screenUV - 0.5)) * 1., 3.) - 0.2);
     float radialCircle = 1. - ( (1. - length(screenUV - 0.5)) - 0.75 );
     radialCircle += strength * 0.5;
-    // textUV = scaleUV(textUV, vec2(radialCircle), textUV - 0.5);
     textUV = scaleUV(textUV, vec2(radialCircle), vec2(0.5));
 
 
     // Perlin noise push
-    // textUV = scaleUV(textUV, vec2(strength), vec2(0.8, 0.5));
     textUV.x -= (strength) * 0.175;
-
-    // tileUV.x += (strength) * 0.01;
-    // tileUV.y += (uTime + strength) * 0.015;
-    // tileUV.y += (uTime) * 0.015;
 
 
     // Falling motion
     textUV.y += (uTime) * 0.09;
-    // textUV.x += (uTime) * 0.019;
 
 
     // Repeat texture (tiling)
     textUV = fract((textUV * 4.) - 0.5);
-    // textUV = fract(textUV * 3.);
 
 
-
+    // Final composite
     float text = msdf(uMap, textUV);
     vec3 color = mix(gradient, textColor, text);
-
-
     float alpha = uTransition;
+
 
     // vec3 tileColor = vec3(tileUV, 1.);
     // color += strength;
@@ -120,6 +104,6 @@ void main()
     // gl_FragColor = vec4(textUV, 1., 1.);
     // gl_FragColor = vec4(vec3(radialCircle), 1.);
     // gl_FragColor = vec4(vec3(strength), 1.);
-    // gl_FragColor = vec4(displacement.rgb, 1.);
+    // gl_FragColor = vec4(vec3(displacement), 1.);
     // gl_FragColor = vec4( msdf(uMap, screenUV) );
 }
